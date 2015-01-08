2345a323874c2bab27118e14baba2b29df5a7114
Jason Aden <jasonaden@gmail.com>
2015-01-08 11:22:58 -0800

    Change Bootstrap from SASS to CSS for complie performance.

--- test/spec/controllers/profile.js
+++ test/spec/controllers/profile.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('angularJsTestTeachApp'));
+  beforeEach(module('testTeachApp'));
 
   var AboutCtrl,
     scope;
